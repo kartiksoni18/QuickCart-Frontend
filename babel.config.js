07c241bb6bd41a0b4fb5d29a37ef677d6bbd0d28
kartiksoni18 <kartik.dots18@gmail.com>
2024-10-11 19:32:03 +0530

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@features': './src/features',
+          '@services': './src/services',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+          '@state': './src/state',
+          '@navigation': './src/navigation',
+          '@types': './src/types',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
---
+++ src/features/auth/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <View className='bg-red-300 flex-1'>
+      <Text>Spalsh</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
